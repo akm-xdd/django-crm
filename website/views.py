3616ceefbcc3c6e35db789b7aa7b55b10ab5377a
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-12-10 22:04:58 +0530

    registration done

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -27,4 +28,20 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have successfully registered')
+            return redirect('home')
+        else:
+            messages.error(request, 'There was an error during registration! Please try again.')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form': form})
+    return render(request, 'register.html', {'form': form})
