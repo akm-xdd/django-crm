f737eded6043505dc25262eadea654555b13d636
Anand Kumar Mishra <ctfu.anand@outlook.com>
2023-12-10 22:52:20 +0530

    records table done

--- website/models.py
+++ website/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Record(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipCode = models.CharField(max_length=6)
+
+    def __str__(self):
+        return (f"{self.firstName} {self.lastName}")
